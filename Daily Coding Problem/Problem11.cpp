c628e5e6e6ac8a52d767a1224b000721ba755699
Jacob Parker <jparker22@wisc.edu>
2019-06-16 23:34:22 -0500

    Problem11 completed

--- Daily Coding Problem/Problem11.cpp
+++ Daily Coding Problem/Problem11.cpp
@@ -11,9 +11,14 @@
 //
 // Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
 
+// TODO add a char to tell whether end of word found
+
 #include <iostream>
 #include <string>
+#include <vector>
 #include <map>
+#include <assert.h>
+#include <stack>
 using namespace std;
 
 class trie
@@ -22,25 +27,30 @@ class trie
     {
 private:
         char letter;
+        unsigned int level;
         map<char, trie_node *> *children;
 public:
         // getters
         char getLetter() { return this->letter; }
+        unsigned int getLevel() { return this->level; }
         map<char, trie_node *> *getChildren() { return this->children; }
         trie_node *getChild(char c)
         {
-            trie_node *result = this->children->find(c);
-            return (result == children->end() ? NULL : result);
+            return (children->find(c) == children->end()) ? NULL : children->at(c);
         }
 
         // setters
         void setLetter(char letter) { this->letter = letter; }
+        void setLevel(unsigned int level) { this->level = level; }
         void setChildren(map<char, trie_node *> *children) { this->children = children; }
-        trie_node *addChild(char letter)
+        trie_node *addChild(char letter, unsigned int level)
         {
             if (children->find(letter) == children->end())
             {
-                trie_node *new_child = new trie_node{letter, NULL};
+                trie_node *new_child = new trie_node;
+                new_child->setLetter(letter);
+                new_child->setLevel(level);
+                new_child->setChildren(new map<char, trie_node*>());
                 children->insert(pair<char, trie_node*>(letter, new_child));
                 return new_child;
             }
@@ -52,43 +62,123 @@ private:
     trie_node *root;  // usually does not have data
 
 public:
-    trie() {}
+    trie() { root = new trie_node; root->setChildren(NULL); }
+    ~trie() {}
 
     trie_node *getRoot() { return this->root; }
+
+    void initRoot()
+    {
+        this->root = new trie_node;
+        this->root->setChildren(NULL);
+    }
 
     // solves the problem at hand
     vector<string> *getPossibleWords(string prefix)
     {
+        trie_node *curr = getRoot();
+        vector<string> *possibleWords = new vector<string>();
+        string word("");
+        for (char curr_c: prefix)
+        {
+            word = word + curr_c;
+            curr = curr->getChild(curr_c);
+            if (curr == NULL) return possibleWords;  // return empty list - substring not in trie
+        }
 
+        // traverse subtree looking for asterisks
+        stack <trie_node *> st;
+        st.push(curr);
+        while (!st.empty())
+        {
+            curr = st.top();
+            st.pop();
+            word = word.substr(0, curr->getLevel()-1) += curr->getLetter();
+            if (curr->getChild('*') != NULL)
+                possibleWords->push_back(string (word));
+            for (auto child = curr->getChildren()->begin(); child != curr->getChildren()->end(); child++)
+            {
+                if (child->second->getLetter() != '*')
+                    st.push(child->second);
+            }
+        }
+        return possibleWords;
     }
 
     // mainly for testing purposes
-    boolean lookup(string word)
+    bool lookup(string word)
     {
         trie_node *curr = getRoot();
         for (char curr_c: word)
         {
-            if ((curr = curr->getChild(curr_c) == NULL)
+            if ((curr = curr->getChild(curr_c)) == NULL)
                 return false;
         }
-        return true;
+        return curr->getChild('*') != NULL;
     }
 
     // add a new word to our dictionary
     void add(string word)
     {
+        unsigned int level = 0;
         trie_node *curr = getRoot(), *tmp;
         for (char curr_c: word)
         {
-            if ((tmp = curr->getChild(curr_c) == NULL)
-                curr = curr->addChild(curr_c);
+            level += 1;
+            if (curr->getChildren() == NULL)
+            {
+                map<char, trie_node *> *ptr = new map<char, trie_node*>();
+                curr->setChildren(ptr);
+                curr = curr->addChild(curr_c, level);
+            }
+            else if ((tmp = curr->getChild(curr_c)) == NULL)
+                curr = curr->addChild(curr_c, level);
             else curr = tmp;
         }
+        curr->addChild('*', level);
     }
 };
 
 
+// test out the trie implementation
 int main(int argc, char *argv[])
 {
+    trie *t = new trie();
+    t->add("word");
+    t->add("words");
+    t->add("world");
+    t->add("sort");
+    t->add("sorting");
+    t->add("happy");
+    t->add("happening");
+    t->add("wonder");
+    t->add("wo");
+    t->add("will");
+    t->add("wouldn't");
+    t->add("apple");
+    t->add("application");
+    t->add("apply");
 
+    cout << "running tests..." << endl << endl;
+
+    assert(t->lookup("word") == true);
+    assert(t->lookup("words") == true);
+    assert(t->lookup("world") == true);
+    assert(t->lookup("sort") == true);
+    assert(t->lookup("sorry") == false);
+
+    // first set of tests
+    string test_w ("wo");
+    vector<string> *result = t->getPossibleWords(test_w);
+
+    cout << "Printing dictionary of possible words for " << test_w << endl;
+    for (auto ans: *result) cout << "word: " << ans << endl;
+    cout << endl;
+    
+    // second set of tests
+    string test_w2 ("app");
+    result = t->getPossibleWords(test_w2);
+
+    cout << "Printing dictionary of possible words for " << test_w2 << endl;
+    for (auto ans: *result) cout << "word: " << ans << endl;
 }
